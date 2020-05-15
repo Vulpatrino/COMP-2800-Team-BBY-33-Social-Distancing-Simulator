eb228dac022129a81e7c49f147efd171d451acfb
wueddyw <wueddyw@gmail.com>
2020-05-15 12:36:04 -0700

    small change

--- script/addScore.js
+++ script/addScore.js
@@ -1,8 +1,7 @@
-function addScore(score, time){
+function addScore(time){
     console.log("Added score");
     firebase.auth().onAuthStateChanged(function (user){
         db.collection("users").doc(user.uid).collection("Scores").add({
-            "score" : score,
             "time" : time
         });
     });
