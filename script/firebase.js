5f27d90b726be19414360b137729c035498334f1
Vulpatrino <jaedonbraun@gmail.com>
2020-05-15 12:14:32 -0700

    Combined Firebase functions into one script

--- script/firebase.js
+++ script/firebase.js
@@ -20,3 +20,24 @@ function getAccountInfo() {
         });
     });
 }
+
+function createUser() {
+
+    // if the user is authenticated, get this "user" object
+    // create this user node(doc) in the datebase users collection
+    console.log("Works")
+    firebase.auth().onAuthStateChanged(function(user) {
+        db.collection("users").doc(user.uid).set({
+            "name":user.displayName, 
+            "email":user.email,
+        });
+    });
+}
+
+function logout(){
+    firebase.auth().signOut().then(function() {
+   console.log('Signed Out');
+ })
+     window.location.replace("index.html");
+     
+ }
