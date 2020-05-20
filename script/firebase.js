99151a8e11988e57c13dd1c921b7b6997950903d
Eric Dam <dam.eric3@gmail.com>
2020-05-20 15:21:35 -0700

    fixed createUser function

--- script/firebase.js
+++ script/firebase.js
@@ -26,9 +26,9 @@ function createUser() {
     // if the user is authenticated, get this "user" object
     // create this user node(doc) in the datebase users collection
     console.log("Works")
+    var button = document.createElement("INPUT");
+    $("#menu").append(button);
     firebase.auth().onAuthStateChanged(function (user) {
-        var button = document.createElement("INPUT");
-        $("#menu").append(button);
         if (user) {
             db.collection("users").doc(user.uid).set({
                 "name": user.displayName,
